1fcbc81b89a616868b95bed248215caf545f505c
Dominic Sherman <dominic.sherman98@gmail.com>
2021-06-01 17:01:50 -0700

    Follow guide

--- next.config.js
+++ next.config.js
@@ -4,5 +4,10 @@ const withMDX = require('@next/mdx')({
 });
 
 module.exports = withMDX({
+  env: {
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    REPO_FULL_NAME: process.env.REPO_FULL_NAME,
+    BASE_BRANCH: process.env.BASE_BRANCH,
+  },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,45 @@
-import * as Options from 'enums/options';
+import { getGithubPreviewProps, parseJson } from 'next-tinacms-github';
+import { GetStaticProps } from 'next';
 
 import { NavBar } from 'components';
 
-export default function Home() {
+export default function Home({ file }) {
+  console.log({ file });
+  const data = file.data;
   return (
     <div className="w-screen h-screen flex flex-col">
       <NavBar />
       <div className="h-screen flex flex-col justify-center items-center w-full">
-        <h1 className="text-3xl font-semibold ml-7">{`Welcome to ${Options.APP_TITLE}`}</h1>
+        <h1 className="text-3xl font-semibold ml-7">{data.title}</h1>
       </div>
     </div>
   );
 }
+
+/**
+ * Fetch data with getStaticProps based on 'preview' mode
+ */
+export const getStaticProps: GetStaticProps = async function ({
+  preview,
+  previewData,
+}) {
+  if (preview) {
+    return getGithubPreviewProps({
+      ...previewData,
+      fileRelativePath: 'content/home.json',
+      parse: parseJson,
+    });
+  }
+
+  return {
+    props: {
+      sourceProvider: null,
+      error: null,
+      preview: false,
+      file: {
+        fileRelativePath: 'content/home.json',
+        data: (await import('../../content/home.json')).default,
+      },
+    },
+  };
+};
